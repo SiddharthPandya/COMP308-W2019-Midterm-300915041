e9d38e964d92ab9293cbe84a752d1875609f23e9
Siddharth Pandya <sid.pandya66@gmail.com>
2019-02-23 15:25:49 -0500

    GET request added for add and edit books

--- server/routes/books.js
+++ server/routes/books.js
@@ -26,9 +26,15 @@ router.get('/', (req, res, next) => {
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    res.render('books/details', {
+      title: 'Add New Book',
+      books: {
+        Title: "",
+        Price: "",
+        Author: "",
+        Genre: ""
+      }
+  });
 
 });
 
@@ -44,9 +50,21 @@ router.post('/add', (req, res, next) => {
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    let id = req.params.id
+
+    book.findById(id, (err,books) => {
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            //show the edit view
+            res.render('books/details',{
+                title: 'Edit Books',
+                books: books
+            });
+        }
+    });
 });
 
 // POST - process the information passed from the details form and update the document
