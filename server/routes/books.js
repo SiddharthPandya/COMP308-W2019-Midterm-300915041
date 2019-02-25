632a0ec4789d5260d3ddae218f24e924db7352c4
Siddharth Pandya <sid.pandya66@gmail.com>
2019-02-24 20:51:43 -0500

    implemented POST request to add a book

--- server/routes/books.js
+++ server/routes/books.js
@@ -9,7 +9,7 @@ let book = require('../models/books');
 /* GET books List page. READ */
 router.get('/', (req, res, next) => {
   // find all books in the books collection
-  book.find( (err, books) => {
+  book.find((err, books) => {
     if (err) {
       return console.error(err);
     }
@@ -26,14 +26,14 @@ router.get('/', (req, res, next) => {
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
 
-    res.render('books/details', {
-      title: 'Add New Book',
-      books: {
-        Title: "",
-        Price: "",
-        Author: "",
-        Genre: ""
-      }
+  res.render('books/details', {
+    title: 'Add New Book',
+    books: {
+      Title: "",
+      Price: "",
+      Author: "",
+      Genre: ""
+    }
   });
 
 });
@@ -41,38 +41,52 @@ router.get('/add', (req, res, next) => {
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  let newBook = book({
+    "Title": req.body.title,
+    "Price": req.body.price,
+    "Author": req.body.author,
+    "Genre": req.body.genre
+  });
+
+  book.create(newBook, (err, book) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    }
+    else {
+      //refresh the books list
+      res.redirect('/books');
+    }
+  });
 
 });
 
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
 
-    let id = req.params.id
+  let id = req.params.id
 
-    book.findById(id, (err,books) => {
-        if(err){
-            console.log(err);
-            res.end(err);
-        }
-        else{
-            //show the edit view
-            res.render('books/details',{
-                title: 'Edit Books',
-                books: books
-            });
-        }
-    });
+  book.findById(id, (err, books) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    }
+    else {
+      //show the edit view
+      res.render('books/details', {
+        title: 'Edit Books',
+        books: books
+      });
+    }
+  });
 });
 
 // POST - process the information passed from the details form and update the document
 router.post('/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  /*****************
+   * ADD CODE HERE *
+   *****************/
 
 });
 
@@ -81,16 +95,16 @@ router.get('/delete/:id', (req, res, next) => {
 
   let id = req.params.id;
 
-  book.remove({_id:id}, (err) => {
-      if(err){
-          console.log(err);
+  book.remove({ _id: id }, (err) => {
+    if (err) {
+      console.log(err);
 
-          res.end(err);
-      }
-      else{
-          //refresh the contact list
-          res.redirect('/books');
-      }
+      res.end(err);
+    }
+    else {
+      //refresh the contact list
+      res.redirect('/books');
+    }
   });
 });
 
